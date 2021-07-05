08983fb466a71cedcf91fde8e4dc1ac14fefd809
Aaron Christianson <ninjaaron@gmail.com>
2021-07-05 10:24:35 +0200

    changes suggested by Jens

--- docker/ubuntu-solr/download-solr.sh
+++ docker/ubuntu-solr/download-solr.sh
@@ -2,5 +2,6 @@
 VERSION="$1"
 SOLR_HOME="solr-$VERSION"
 wget "http://mirror.23media.de/apache/lucene/solr/$VERSION/$SOLR_HOME.tgz" || exit 1
+sha512sum --check shasum.txt --ignore-missing
 tar xaf "$SOLR_HOME.tgz" || exit 1
 rm "$SOLR_HOME.tgz"

40744084d13b784d0c644ffc00bea7f078d38cd6
Aaron Christianson <ninjaaron@gmail.com>
2021-07-07 22:35:21 +0200

    ignore SSL warnings for now

--- example-client/cli_client.py
+++ example-client/cli_client.py
@@ -5,7 +5,8 @@ import requests
 import pprint
 
 session = requests.Session()
-domain, query = sys.argv[1:3]
+domain = sys.argv[1]
+query = sys.stdin.read()
 URL = f"{domain}/api/"
-resp = session.get(URL + urllib.parse.quote(query)) #, verify=False)
+resp = session.get(URL + urllib.parse.quote(query), verify=False)
 pprint.pprint(resp.json())

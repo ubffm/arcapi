c003c226e2f0c761974db6aca2759b6866fbcca0
Aaron Christianson <ninjaaron@gmail.com>
2022-04-22 09:31:15 +0200

    update solr version

--- docker/ubuntu-solr/download-solr.sh
+++ docker/ubuntu-solr/download-solr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 VERSION="$1"
 SOLR_HOME="solr-$VERSION"
-wget "http://mirror.23media.de/apache/lucene/solr/$VERSION/$SOLR_HOME.tgz" || exit 1
+wget "https://dlcdn.apache.org/lucene/solr/$VERSION/$SOLR_HOME.tgz" || exit 1
 sha512sum --check shasum.txt --ignore-missing || exit 1
 tar xaf "$SOLR_HOME.tgz" || exit 1
 rm "$SOLR_HOME.tgz"

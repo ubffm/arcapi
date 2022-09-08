c4e20cf3bbad7a036fbf966130315996b6d42fe6
Aaron Christianson <ninjaaron@gmail.com>
2022-09-08 15:20:13 +0200

    added cython and wheel to requirements.txt

--- docker/api/project_setup.sh
+++ docker/api/project_setup.sh
@@ -3,5 +3,5 @@ mkdir -p ~/.config/deromanize/
 mv ./config.yml ~/.config/deromanize/
 python3 -m venv venv
 venv/bin/pip install -U pip
-venv/bin/pip installa -r requirements.txt
+venv/bin/pip install -r requirements.txt
 venv/bin/pip install "git+https:///github.com/ubffm/arcapi.git"

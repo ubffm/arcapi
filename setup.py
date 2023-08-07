dc482db5ec4bca3057bee38fcec811971843a0e7
Aaron Christianson <ninjaaron@gmail.com>
2023-08-07 11:34:38 +0200

    updating manifest

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ setup(
     description="web API for Revrit project",
     #long_description=open("README.rst").read(),
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "pica_parse @ git+https://github.com/FID-Judaica/pica_parse.py.git",
         "arc @ git+https://github.com/FID-Judaica/goethe-university-library-arc.git",
         "compose-struct @ git+https://github.com/ninjaaron/compose-struct.git",
-        "cython",
-        "HspellPy",
+        # "cython==0.29.36",
+        # "HspellPy",
     ],
 )
